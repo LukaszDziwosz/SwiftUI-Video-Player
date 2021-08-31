a89d1739d6e717dd7696f75019b4294035bdfe48
LukaszDziwosz <35117633+LukaszDziwosz@users.noreply.github.com>
2021-08-31 21:58:52 +0200

    Initial Commit

---
+++ SwiftUI-VideoPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-VideoPlayer
+//
+//  Created by Lukasz Dziwosz on 31/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-VideoPlayer/SwiftUI_VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideoPlayerApp.swift
+//  SwiftUI-VideoPlayer
+//
+//  Created by Lukasz Dziwosz on 31/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
